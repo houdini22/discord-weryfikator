081309645bf0106c9a48be0efd1ac93ff1bd9998
lukezord <lukezord@users.noreply.github.com>
2017-09-04 00:33:05 +0200

    Update ramowka.vue

--- src/components/views/ramowka.vue
+++ src/components/views/ramowka.vue
@@ -1,1 +1,40 @@
+<template>
+    <div class="page page-ramowka">
+        <header-component></header-component>
+        <div class="container">
+            <h3>Ramówka</h3>
+            <div class="ramowka-container">
+                
+            </div>
+        </div>
+        <footer-component></footer-component>
+    </div>
+</template>
+
+<script>
+  import HeaderComponent from '../partials/Header.vue'
+  import FooterComponent from '../partials/Footer.vue'
+  export default {
+    components: {
+      'header-component': HeaderComponent,
+      'footer-component': FooterComponent
+    },
+    name: 'servers'
+  }
+</script>
+
+<style scoped lang="scss">
+    .servers-container {
+        text-align: center;
+        .servers {
+            li {
+                display: inline-block;
+                margin-right: 15px;
+                &:last-child {
+                    margin: 0;
+                }
+            }
+        }
+    }
+</style>
 
